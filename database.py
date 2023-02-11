c08e223aa6d9ffacd225d1799336424930cd14d9
embeddedapps <118255837+embeddedapps@users.noreply.github.com>
2023-02-11 23:58:30 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,35 @@
+import os
+import streamlit as st
+from deta import Deta
+from dotenv import load_dotenv
+
+# #load the environment variables
+load_dotenv(".env")
+DETA_KEY = st.secrets('KEY')
+#DETA_KEY= 'd061eczc_2bi3UJ9a98PvDdmtYaciksXwJbuqoXi8'
+#initialize with a product key
+deta = Deta(DETA_KEY)
+
+#connecting to database
+db = deta.Base('streamlit_db')
+def insert_user(username, name, password):
+    return db.put({'key':username, 'name': name, 'password': password})
+
+#for inserting users to db
+#insert_user('gkumar', 'Gaurav Kumar', '123')
+
+# #for fetching data from db
+def fetch_all_users():
+     res = db.fetch()
+     return res.items
+#print(fetch_all_users())
+
+def get_user(username):
+    return db.get(username)
+
+def update_user(username, updates):
+    return db.update(updates, username)
+
+def delete_user(username):
+    return db.delete(username)
+#delete_user('gkumar')
--- weatherwebapp.py
+++ weatherwebapp.py
@@ -6,107 +6,129 @@ from streamlit_option_menu import option_menu
 #from pandas_profiling import ProfileReport
 import time
 import pickle
+
 from pathlib import Path
-
 import streamlit_authenticator as stauth
+import database as db
 
-#----------------------------User Authentication-------------------------#
+st.set_page_config(page_title="Weather App", page_icon=":bar_chart:", layout="wide")
 
-
-#-------------------------------------------------------------------------#
-
-#loading the model
-#weather_model = pickle.load(open('C:\Users\gkuma\Documents\Python\DeployedModel\weathercloudmodel\weather_model.sav', 'rb'));
+#----------------------------User Login-------------------------#
+users = db.fetch_all_users()
 
 
+usernames = [user["key"] for user in users]
+names = [user["name"] for user in users]
+hashed_passwords = [user["password"] for user in users]
 
-#sidebar for navigation
-with st.sidebar:
-    selected = option_menu('Welcome To Our Model Prediction',
-    ['Home','Data','Weather Prediction','Conversion'],
-    icons = ['house','bar-chart','cloud-haze2','arrows-angle-contract'],  #through bootstrap
-    default_index=0
-    )
+credentials = {"usernames":{}}
+for uname,name,pwd in zip(usernames, names, hashed_passwords):
+    user_dict = {"name":name,"password":pwd}
+    credentials["usernames"].update({uname:user_dict})
 
-if(selected == 'Home'):
-    #page title
-    st.title('Secure Communication Based IoT Weather Forecasting')
-    with st.expander('About this Project'):
-        st.subheader('Provides refined and targeted forecasts as it involve the integration of advanced technologies such as machine learning, big data analysis, and cloud computing, to produce more accurate and localized forecasts.')
-    #st.image('')
-   # if st.button('Click to continue',):
-   # slider = st.slider('text', 0, 130, 25)
+#authentication  
+authenticator = stauth.Authenticate(credentials, "app_home", "auth", cookie_expiry_days=30)
 
-if(selected == 'Data'):
-    st.header('Data profile')
-    uploaded_file = st.file_uploader("Choose a file")
+name, authentication_status, username = authenticator.login("Login", "main")
 
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        #slider completion
-        my_bar = st.progress(0)
-        for percent_complete in range(100):
-            time.sleep(0.01)
-            my_bar.progress(percent_complete + 1)
-        st.subheader('DataFrame')
-        st.write(df)
-        st.subheader('Descriptive Statistics')
-        st.write(df.describe())
-   
-#      df = pd.read_csv('C:/Users/gkuma/Downloads/JupyterNotebook/Datasets/drug200.csv')
-#      profile = ProfileReport(df,title='Profile Report')
+if authentication_status == False:
+    st.error("Username/password is incorrect")
 
+if authentication_status == None:
+    st.warning("Please enter your username and password")
 
-if(selected == 'Weather Prediction'):
-    #page title
-    st.title('Weather Prediction Using ML')
+if authentication_status == True:
+    #loading the model
+    #weather_model = pickle.load(open('C:\Users\gkuma\Documents\Python\DeployedModel\weathercloudmodel\weather_model.sav', 'rb'));
+ 
+    #sidebar for navigation
+    authenticator.logout("Logout","sidebar")
+    with st.sidebar:
+        selected = option_menu('Welcome To Our Model Prediction',
+        ['Home','Data','Weather Prediction','Conversion'],
+        icons = ['house','bar-chart','cloud-haze2','arrows-angle-contract'],  #through bootstrap
+        default_index=0
+        )
+
+    if(selected == 'Home'):
+        #page title
+        st.title('Secure Communication Based IoT Weather Forecasting')
+        with st.expander('About this Project'):
+            st.subheader('Provides refined and targeted forecasts as it involve the integration of advanced technologies such as machine learning, big data analysis, and cloud computing, to produce more accurate and localized forecasts.')
+        #st.image('')
+    # if st.button('Click to continue',):
+    # slider = st.slider('text', 0, 130, 25)
+
+    if(selected == 'Data'):
+        st.header('Data profile')
+        uploaded_file = st.file_uploader("Choose a file")
+
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+            #slider completion
+            my_bar = st.progress(0)
+            for percent_complete in range(100):
+                time.sleep(0.01)
+                my_bar.progress(percent_complete + 1)
+            st.subheader('DataFrame')
+            st.write(df)
+            st.subheader('Descriptive Statistics')
+            st.write(df.describe())
     
-    #creating columns
-    col1, col2, col3 = st.columns(3)
+    #      df = pd.read_csv('C:/Users/gkuma/Downloads/JupyterNotebook/Datasets/drug200.csv')
+    #      profile = ProfileReport(df,title='Profile Report')
 
-    with col1:
-        Temperature = st.text_input('Temperature level')
 
-    with col2:
-        Humidity = st.text_input('Humidity level')
-    with col3:
-        Pressure = st.text_input('Pressure Level')
-
-    #creating a null string to store outcome result
-    weather_result = ''
-
-    #creating a button for prediction
-    if st.button('Weather Prediction Result'):
-        #weather_predict = weather_model.predict([[Temperature,Humidity,Pressure]])  #weather_model function in which model is defined
-
-       # if(weather_predict[0]==1):
-            weather_result = 'It will rain'
-       # else:
-            weather_result = 'No rainfall'
-
-if(selected == 'Conversion'):
-    st.header('Converting Celsius to Farenheit and Vice-versa')
-    def fh_to_cel():
-        st.session_state.cel = (st.session_state.fh/0.5556 - 32)
-    def cel_to_fh():
-        st.session_state.fh = (st.session_state.cel*1.8 + 32)
-    
-    col1, spacer, col2 = st.columns([2,1,2])
-    with col1:
-        fahrenheit = st.number_input("Fahrenheit:", key = "fh", on_change = fh_to_cel)
-    with col2:
-          celsius = st.number_input("Celsius:", key = "cel", on_change = cel_to_fh)
-    st.button('Reset')
+    if(selected == 'Weather Prediction'):
+        #page title
+        st.title('Weather Prediction Using ML')
         
+        #creating columns
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            Temperature = st.text_input('Temperature level')
+
+        with col2:
+            Humidity = st.text_input('Humidity level')
+        with col3:
+            Pressure = st.text_input('Pressure Level')
+
+        #creating a null string to store outcome result
+        weather_result = ''
+
+        #creating a button for prediction
+        if st.button('Weather Prediction Result'):
+            #weather_predict = weather_model.predict([[Temperature,Humidity,Pressure]])  #weather_model function in which model is defined
+
+        # if(weather_predict[0]==1):
+                weather_result = 'It will rain'
+        # else:
+                weather_result = 'No rainfall'
+        #st.snow()
+    if(selected == 'Conversion'):
+        st.header('Converting Celsius to Farenheit and Vice-versa')
+        def fh_to_cel():
+            st.session_state.cel = (st.session_state.fh/0.5556 - 32)
+        def cel_to_fh():
+            st.session_state.fh = (st.session_state.cel*1.8 + 32)
+        
+        col1, spacer, col2 = st.columns([2,1,2])
+        with col1:
+            fahrenheit = st.number_input("Fahrenheit:", key = "fh", on_change = fh_to_cel)
+        with col2:
+            celsius = st.number_input("Celsius:", key = "cel", on_change = cel_to_fh)
+        st.button('Reset')
+            
 
 
-    
-###########################################################################footer ctrl+/
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+        
+ ###########################################################################footer ctrl+/
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
