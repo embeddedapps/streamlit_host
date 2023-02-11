1d20124d0176c886191e60a46190c4c284a10dec
embeddedapps <118255837+embeddedapps@users.noreply.github.com>
2023-02-12 00:19:44 +0530

    Add files via upload

--- weatherwebapp.py
+++ weatherwebapp.py
@@ -124,11 +124,11 @@ if authentication_status == True:
 
         
  ###########################################################################footer ctrl+/
-    hide_st_style = """
+hide_st_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 header {visibility: hidden;}
                 </style>
                 """
-    st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
